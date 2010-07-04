55bb22d6bc10f172f2fd53205ec0e129e1251b36
Chen Guo <chenguo4@ucla.edu>
2010-07-04 00:32:25 -0700

    Add dependency graph and file headers.
    * src/dgraph.h: New file. Header file for dependency graph.
    * src/file.h: New file. Header file for file depdencies.

---
+++ src/dgraph.h
@@ -0,0 +1,59 @@
+/* Header for dependency graph. 
+   Copyright (C) 2010 Chen Guo
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Written by Chen Guo, chenguo4@ucla.edu.  */
+
+#ifndef DGRAPH_H
+#define DGRAPH_H
+
+
+struct dg_node;
+
+/* List of graph nodes. */
+struct dg_list
+{
+  struct dg_node *node;          /* Dependency graph node. */
+  struct dg_list *next;          /* Next node in list. */
+};
+
+/* Dependency graph node. */
+struct dg_node
+{
+  int type;                      /* Type of command. */
+  int dependencies;              /* Dependencies. */
+  struct dg_list *dependents;    /* Dependent nodes. */
+  struct dg_node *parent;        /* Parent node. */
+  /* Command. */
+  /* Loop control. */
+  unsigned long iteration;       /* Iteration of parent loop. */
+  int nest;                      /* Nest level of command. */
+  /* Frontier control. */
+  struct dg_node *next;          /* Next node in frontier. */
+  struct dg_node *prev;          /* Previous node in frontier. */
+};
+
+/* Frontier. */
+struct dg_frontier
+{
+  struct dg_node *run_list;      /* Running/runnable commands. */
+  struct dg_node *run_next;      /* Next runnable command. */
+  struct dg_node *tail;          /* Tail of frontier. */
+  int eof;                       /* End of file flag. */
+  pthread_mutex_t *dg_lock;      /* Directed graph lock. */
+  pthread_cond_t *dg_cond;       /* Conditional variable for lock. */
+};
+
+#endif
---
+++ src/file.h
@@ -0,0 +1,50 @@
+/* Header for file list and dependency. 
+   Copyright (C) 2010 Chen Guo
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Written by Chen Guo, chenguo4@ucla.edu.  */
+
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdbool.h>
+
+#include "dgraph.h"
+
+/* Access type. */
+enum
+  {
+    READ_ACCESS,
+    WRITE_ACCESS,
+    RW_ACCESS
+  };
+
+/* File being accessed by one or more commands. */
+struct f_file
+{
+  /* TDOO: inode */
+  char *path;                    /* Absolute path to the file. */
+  struct f_access *accessors;    /* Commands accessing file. */
+  struct f_file *next;           /* Next file. */
+};
+
+/* File accessor. */
+struct f_access
+{
+  int access;                    /* Access type. */
+  struct dg_node *node;          /* Graph node of command. */
+};
+
+#endif
